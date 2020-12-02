2c4566c8fe61783be5eb27c264dcf46cf06160a6
kuti gbolahan <kutigbolahan.ab@gmail.com>
2020-12-02 10:55:44 -0800

    Initial Commit

---
+++ Swift_CheckOut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift_CheckOut
+//
+//  Created by Mac on 12/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
