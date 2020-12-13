cc3a87f14173db1c34f1365f46dbf042b0fa9365
kuti gbolahan <kutigbolahan.ab@gmail.com>
2020-12-13 08:16:52 -0800

    nav view

--- Swift_CheckOut/ContentView.swift
+++ Swift_CheckOut/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }.navigationTitle("MENU") 
+        }
     }
 }
 
