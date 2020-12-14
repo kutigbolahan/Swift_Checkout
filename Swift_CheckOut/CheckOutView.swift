176aad4a4daf7d29e996aac2f0061d775a4ceaa4
kuti gbolahan <kutigbolahan.ab@gmail.com>
2020-12-14 11:32:51 -0800

    percentage

--- Swift_CheckOut/CheckOutView.swift
+++ Swift_CheckOut/CheckOutView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct CheckOutView: View {
     @EnvironmentObject var order: Order
+    
     static let paymentTypes = ["Cash","Credit Card","CheckoutPoints"]
+    static let tipAmounts = [10,15,20,25,0]
+    
     @State private var paymentType = 0
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
+    @State private var tipAmount = 1
     var body: some View {
         Form{
             Section{
@@ -21,13 +25,25 @@ struct CheckOutView: View {
                         Text(Self.paymentTypes[$0])
                     }
                 }
-                Toggle(isOn: $addLoyaltyDetails) {
+                Toggle(isOn: $addLoyaltyDetails.animation()) {
                     Text("Add checkout loyalty card")
                 }
                 if addLoyaltyDetails{
                 TextField("Enter your checkout ID", text:$loyaltyNumber)
                 }
             }
+            Section(header: Text("Add a tip?")){
+                Picker("Percentage:", selection:$tipAmount){
+                    ForEach(0..<Self.tipAmounts.count){
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            Section(header:Text("TOTAL: $100")){
+                Button("Confirm order"){}
+            }
+            
+            
         }
         .navigationBarTitle(Text("Payment"),displayMode: .inline)
     }
